86beaa6d29ba05155cc6eb6d34d054dbc345e8e6
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-02-03 14:07:23 -0800

    Refactored Expenses out of App to clean up code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate.js';
 
 export default function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
       <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
+        <ExpenseDate date={props.date} />
       </div>
       <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
